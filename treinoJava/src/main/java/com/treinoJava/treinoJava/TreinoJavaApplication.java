498b2dca1c2b520aedde64646ec3697bac54b802
Anderson Bolsanelli <112144985+anderson-159@users.noreply.github.com>
2024-07-29 11:44:00 -0300

    Add files via upload

---
+++ treinoJava/src/main/java/com/treinoJava/treinoJava/TreinoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.treinoJava.treinoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoJavaApplication.class, args);
+	}
+
+}
---
+++ treinoJava/src/test/java/com/treinoJava/treinoJava/TreinoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinoJava.treinoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
